83cff66aace62f2965a26547e1a1fbd8d79dc2d2
Dioma <dioma@gmail.com>
2020-08-03 11:42:32 +0300

    Updates

---
+++ precache-manifest.7540d136e3231265f08308b0efc81b3d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ad55d4ac64813af375e5381e97833f",
+    "url": "/Parallax/index.html"
+  },
+  {
+    "revision": "e1b6ad059885ce801592",
+    "url": "/Parallax/static/css/main.2e370b43.chunk.css"
+  },
+  {
+    "revision": "62fa51e9cb70924ea37e",
+    "url": "/Parallax/static/js/2.7405d706.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Parallax/static/js/2.7405d706.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b6ad059885ce801592",
+    "url": "/Parallax/static/js/main.e317ade1.chunk.js"
+  },
+  {
+    "revision": "5efdc97678762ee12670",
+    "url": "/Parallax/static/js/runtime-main.2c3fec38.js"
+  },
+  {
+    "revision": "ce521a2dcebdff98551242505a7d3ad7",
+    "url": "/Parallax/static/media/Ship-Landscape.ce521a2d.svg"
+  },
+  {
+    "revision": "5546c7c6ef89997c5735d5b2aa91b21b",
+    "url": "/Parallax/static/media/Stars.5546c7c6.svg"
+  },
+  {
+    "revision": "d032006ca7eaf17b546bb99cd56f79ae",
+    "url": "/Parallax/static/media/eclipse.d032006c.svg"
+  },
+  {
+    "revision": "60c8a4bfb44e5be263c3981bbf6877a4",
+    "url": "/Parallax/static/media/moon.60c8a4bf.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Parallax/precache-manifest.7540d136e3231265f08308b0efc81b3d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Parallax/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
