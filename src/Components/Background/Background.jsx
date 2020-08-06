50ecbd61503d5d801f3d7fed92c01efc7b8dff33
Dioma <dioma@gmail.com>
2020-08-06 12:15:27 +0300

    responsive

--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -17,33 +17,33 @@ const Background = () => {
             />
 
             <ParallaxLayer offset={0.1} speed={-0.4} style={{ pointerEvents: 'none' }}>
-                <img src={eclipse} style={{ width: '15%', marginLeft: '80%', opacity: 0.8 }} />
+                <img alt="img" src={eclipse} style={{ width: '15%', marginLeft: '80%', opacity: 0.8 }} />
             </ParallaxLayer>
 
             <ParallaxLayer offset={0.24} speed={0.6} style={{ opacity: 0.1 }}>
-                <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
-                <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
+                <img alt="img" src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
+                <img alt="img" src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
             </ParallaxLayer>
 
             <ParallaxLayer offset={0.2} speed={0.5} style={{ opacity: 0.1 }}>
-                <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
-                <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
+                <img alt="img" src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
+                <img alt="img" src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
             </ParallaxLayer>
 
             <ParallaxLayer offset={0.26} speed={0.2} style={{ opacity: 0.2 }}>
-                <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
-                <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '75%' }} />
+                <img alt="img" src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
+                <img alt="img" src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '75%' }} />
             </ParallaxLayer>
 
             <ParallaxLayer offset={0.21} speed={-0.1} style={{ opacity: 0.4 }}>
-                <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '60%' }} />
-                <img src={url('cloud')} style={{ display: 'block', width: '25%', marginLeft: '30%' }} />
-                <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
+                <img alt="img" src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '60%' }} />
+                <img alt="img" src={url('cloud')} style={{ display: 'block', width: '25%', marginLeft: '30%' }} />
+                <img alt="img" src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
             </ParallaxLayer>
 
             <ParallaxLayer offset={0.3} speed={0.4} style={{ opacity: 0.6 }}>
-                <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '5%' }} />
-                <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
+                <img alt="img" src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '5%' }} />
+                <img alt="img" src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
             </ParallaxLayer>
 
         </div>
